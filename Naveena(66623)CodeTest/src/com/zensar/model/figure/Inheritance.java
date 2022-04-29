8aac2725b197339d1c371c1b40a6085e98d00a7d
navinathota <102963902+navinathota@users.noreply.github.com>
2022-04-29 09:28:34 +0530

    Update Inheritance.java

--- Naveena(66623)CodeTest/src/com/zensar/model/figure/Inheritance.java
+++ Naveena(66623)CodeTest/src/com/zensar/model/figure/Inheritance.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 
 public class Inheritance  {
 
-	/*public void main(String[] args) {
-		Parallelogram p1=new Parallelogram(10,20,90);
-		System.out.println(p1.area());
-}*/
-	public static void main (String[] args) {
+		public static void main (String[] args) {
+			
 		Scanner s= new Scanner(System.in);
 		System.out.println("Enter input");
 		double dimension1=s.nextDouble();
@@ -18,8 +15,8 @@ public class Inheritance  {
 		
 		Parallelogram p=new Parallelogram(dimension1,dimension2);
 		System.out.println("Area of parallelogram is:"+p.area());
-		Picture pc=new Picture();
 		
+		Picture pc=new Picture();
 		boolean result=pc.add(p);
 		System.out.println(result);
 	}
